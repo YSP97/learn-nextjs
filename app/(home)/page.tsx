84a4ad73ac04f360d872b72d7475555e1ae74b83
YSP97 <pus9717@gmail.com>
2025-04-17 17:22:28 +0900

    [Data Fetching 학습] - Parallel Request

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,20 +1,29 @@
 import { Metadata } from 'next';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Home',
 };
-const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+export const API_URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
 
 async function getMovies() {
   // 백엔드 첫 번째 API 요청이 느린 환경의 상황을 가정하여 아래와 같이 시간을 걸어줌
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  // await new Promise((resolve) => setTimeout(resolve, 5000));
   console.log('im fetching'); // 서버컴포넌트이므로 프론트엔드에서는 콘솔창에 출력되지 않음(백엔드에서만 출력됨!)
-  const respone = await fetch(URL);
+  const respone = await fetch(API_URL);
   const json = await respone.json();
   return json;
 }
 
 export default async function HomePage() {
   const movies = await getMovies();
-  return <>{JSON.stringify(movies)}</>;
+  return (
+    <>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </>
+  );
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,27 @@
-export default function MovieDetail({
+import { API_URL } from '../../../(home)/page';
+
+async function getMovie(id: string) {
+  await new Promise((resolve) => setTimeout(resolve, 2000)); // 백엔드 API 요청이 느린 경우를 가정하여 5초의 딜레이 설정
+  const response = await fetch(`${API_URL}/${id}`);
+
+  return response.json();
+}
+
+async function getVideos(id: string) {
+  console.log(`Fetching Movies: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 2000)); // 백엔드 API 요청이 느린 경우를 가정하여 5초의 딜레이 설정
+  const response = await fetch(`${API_URL}/${id}/videos`);
+
+  return response.json();
+}
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <h1> Movies {id} </h1>;
+  console.log('Start Fetching');
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  console.log('End Fetching');
+  return <h1> {movie.title} </h1>;
 }
