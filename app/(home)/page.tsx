8e69d1dde0afc4b2e34ca2ef505193092cf933f2
YSP97 <pus9717@gmail.com>
2025-04-17 16:29:18 +0900

    [Data Fetching 학습] - Client Side fetch

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,21 @@
-export const metadata = {
-  title: 'Home',
-};
+'use client';
 
-export default function Hello() {
-  return (
-    <>
-      <h1>Home Page</h1>
-    </>
-  );
+import { useEffect, useState } from 'react';
+
+export default function Page() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      'https://nomad-movies.nomadcoders.workers.dev/movies'
+    );
+
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  });
+  return <>{isLoading ? 'Loading...' : JSON.stringify(movies)}</>;
 }
