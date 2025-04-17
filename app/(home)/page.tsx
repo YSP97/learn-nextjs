d9160ec895891573db637cd94419169fefb2f0ba
YSP97 <pus9717@gmail.com>
2025-04-17 16:43:23 +0900

    [Data Fetching 학습] - Server Side fetch data2 (첫 번째 fetch만 API를 요청하고 두 번째 부터는 이미 fetch된 데이터가 존재하여 요청하지 않음)

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,6 +6,7 @@ export const metadata: Metadata = {
 const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
 
 async function getMovies() {
+  console.log('im fetching'); // 서버컴포넌트이므로 프론트엔드에서는 콘솔창에 출력되지 않음(백엔드에서만 출력됨!)
   const respone = await fetch(URL);
   const json = await respone.json();
   return json;
