1f8f8d326c2e6b91c42a9694f35fc1d537d4f718
YSP97 <pus9717@gmail.com>
2025-04-21 16:10:52 +0900

    [Data Fetching 학습] - Suspense(+fallback)

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,27 +1,22 @@
+import { Suspense } from 'react';
 import { API_URL } from '../../../(home)/page';
-
-async function getMovie(id: string) {
-  await new Promise((resolve) => setTimeout(resolve, 2000)); // 백엔드 API 요청이 느린 경우를 가정하여 5초의 딜레이 설정
-  const response = await fetch(`${API_URL}/${id}`);
-
-  return response.json();
-}
-
-async function getVideos(id: string) {
-  console.log(`Fetching Movies: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 2000)); // 백엔드 API 요청이 느린 경우를 가정하여 5초의 딜레이 설정
-  const response = await fetch(`${API_URL}/${id}/videos`);
-
-  return response.json();
-}
+import MovieInfo from '../../../../components/movie-info';
+import MovieVideos from '../../../../components/movie-videos';
 
 export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  console.log('Start Fetching');
-  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
-  console.log('End Fetching');
-  return <h1> {movie.title} </h1>;
+  return (
+    <div>
+      {/* 두 컴포넌트 모두 await해줘야하는 비동기 컴포넌트이다 */}
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
---
+++ components/movie-videos.tsx
@@ -0,0 +1,13 @@
+import { API_URL } from '../app/(home)/page';
+
+async function getVideos(id: string) {
+  await new Promise((resolve) => setTimeout(resolve, 10000)); // 백엔드 API 요청이 느린 경우를 가정하여 10초의 딜레이 설정
+  const response = await fetch(`${API_URL}/${id}/videos`);
+
+  return response.json();
+}
+
+export default async function MovieVideos({ id }: { id: string }) {
+  const videos = await getVideos(id);
+  return <h6>{JSON.stringify(videos)}</h6>;
+}
