01773f87934e25100ed23c27851f5ef4ed6fb513
YSP97 <pus9717@gmail.com>
2025-04-21 16:29:08 +0900

    [Data Fetching 학습] - Error Handling

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -2,9 +2,10 @@ import { API_URL } from '../app/(home)/page';
 
 async function getVideos(id: string) {
   await new Promise((resolve) => setTimeout(resolve, 10000)); // 백엔드 API 요청이 느린 경우를 가정하여 10초의 딜레이 설정
-  const response = await fetch(`${API_URL}/${id}/videos`);
+  throw new Error ('에러...') // 에러 발생시
+  // const response = await fetch(`${API_URL}/${id}/videos`);
 
-  return response.json();
+  // return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
