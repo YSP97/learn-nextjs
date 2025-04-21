a246e466d634312f659f3343eec15b9a0a21f447
YSP97 <pus9717@gmail.com>
2025-04-21 17:25:35 +0900

    [Deployment] - Component, Page styling

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,14 +1,24 @@
 import { API_URL } from '../app/(home)/page';
+import styles from '../styles/movie-videos.module.css';
 
 async function getVideos(id: string) {
-  await new Promise((resolve) => setTimeout(resolve, 10000)); // 백엔드 API 요청이 느린 경우를 가정하여 10초의 딜레이 설정
-  throw new Error ('에러...') // 에러 발생시
-  // const response = await fetch(`${API_URL}/${id}/videos`);
-
-  // return response.json();
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
-  return <h6>{JSON.stringify(videos)}</h6>;
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title={video.name}
+        />
+      ))}
+    </div>
+  );
 }
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,4 +1,5 @@
 'use client'; // CSR로 바뀐 것 같지만? 사실 아님 SSR임 이 컴포넌트에게 interactive해야한다고 선언
+import styles from '../styles/navigation.module.css';
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -6,8 +7,8 @@ import { usePathname } from 'next/navigation';
 export default function Navigation() {
   const path = usePathname();
   return (
-    <nav>
-      <ul>
+    <nav className={styles.nav}>
+      <ul className={styles.list}>
         <li>
           <Link href="/">Home</Link>
           {path === '/' ? '❤️' : ''}
