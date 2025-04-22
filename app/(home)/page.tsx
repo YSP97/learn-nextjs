6c55d3524c8f9d538be28d49957d3c47552eca0f
YSP97 <pus9717@gmail.com>
2025-04-22 16:29:01 +0900

    build 오류 수정

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,11 +2,11 @@ import { Metadata } from 'next';
 import Link from 'next/link';
 import Movie from '../../components/movie';
 import styles from '../../styles/home.module.css';
+import { API_URL } from '../constants';
 
 export const metadata: Metadata = {
   title: 'Home',
 };
-export const API_URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
 
 async function getMovies() {
   const respone = await fetch(API_URL);
