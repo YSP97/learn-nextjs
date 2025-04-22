fc3d62a48dd5a1bc6fd135616712f09d00f2ae11
YSP97 <pus9717@gmail.com>
2025-04-22 16:17:44 +0900

    [Deployment] - Dynamic Metadata    - generateMetadata 함수를 사용

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,9 +9,6 @@ export const metadata: Metadata = {
 export const API_URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
 
 async function getMovies() {
-  // 백엔드 첫 번째 API 요청이 느린 환경의 상황을 가정하여 아래와 같이 시간을 걸어줌
-  // await new Promise((resolve) => setTimeout(resolve, 5000));
-  console.log('im fetching'); // 서버컴포넌트이므로 프론트엔드에서는 콘솔창에 출력되지 않음(백엔드에서만 출력됨!)
   const respone = await fetch(API_URL);
   const json = await respone.json();
   return json;
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,13 +1,21 @@
 import { Suspense } from 'react';
 import { API_URL } from '../../../(home)/page';
-import MovieInfo from '../../../../components/movie-info';
+import MovieInfo, { getMovie } from '../../../../components/movie-info';
 import MovieVideos from '../../../../components/movie-videos';
-
-export default async function MovieDetail({
-  params: { id },
-}: {
+import { title } from 'process';
+interface IParams {
   params: { id: string };
-}) {
+}
+
+export async function generateMetadata({ params: { id } }: IParams) {
+  const movie = await getMovie(id);
+  // 13버전부터는 next.js가 fetch한 데이터를 자동으로 캐싱함 -> 따라서, 두 번 fetch하는 것이 아님
+  return {
+    title: movie.title,
+  };
+}
+
+export default async function MovieDetail({ params: { id } }: IParams) {
   return (
     <div>
       {/* 두 컴포넌트 모두 await해줘야하는 비동기 컴포넌트이다 */}
